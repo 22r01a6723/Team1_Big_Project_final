25862ea96332221ad45c7bbfb99ca2fbf63b0f6d
poojitha <poojitha.gandla@smarsh.com>
2025-09-10 15:53:34 +0530

    added application logs for every microservice

--- Compliance/src/main/java/com/smarsh/compliance/controller/TenantController.java
+++ Compliance/src/main/java/com/smarsh/compliance/controller/TenantController.java
@@ -1,12 +1,19 @@
 package com.smarsh.compliance.controller;
 
-import com.smarsh.compliance.entity.Tenant;
-import com.smarsh.compliance.service.TenantService;
-import org.springframework.web.bind.annotation.*;
-
 import java.util.List;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.smarsh.compliance.entity.Tenant;
+import com.smarsh.compliance.service.TenantService;
+
+import lombok.extern.slf4j.Slf4j;
+
 @RestController
+@Slf4j
 public class TenantController {
 
     private TenantService tenantService;
@@ -17,12 +24,15 @@ public class TenantController {
 
     @PostMapping("/api/public/tenant")
     public org.springframework.http.ResponseEntity<String> addTenant(@RequestBody Tenant tenant) {
+        log.info("START addTenant tenantId={}", tenant.getTenantId());
         String result = tenantService.addTenant(tenant);
+        log.info("END addTenant tenantId={} result={} ", tenant.getTenantId(), result);
         return org.springframework.http.ResponseEntity.ok(result);
     }
 
     @GetMapping("/api/public/tenant")
     public org.springframework.http.ResponseEntity<List<Tenant>> getAllTenant() {
+        log.info("FETCH getAllTenant");
         List<Tenant> tenants = tenantService.getAllTenant();
         return org.springframework.http.ResponseEntity.ok(tenants);
     }
